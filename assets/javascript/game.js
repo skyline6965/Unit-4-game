5d6c36129ff80e0a4fdc8ee63f705e3b96fef570
Kyle Barnhouse <skyline6965@gmail.com>
2019-09-11 16:43:59 -0500

    added car pictures, made intro, working on rest of divs

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// starting random number
+// wins, win counter, 
+// losses, losses counter,
+// The random number shown at the start of the game should be between 19 - 120.
+// Each crystal should have a random hidden value between 1 - 12.
+
+
+// 
